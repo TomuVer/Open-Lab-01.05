8e9c356ae78a79c8cec03307b53feb2272bfcb32
TomuVer <113421396+TomuVer@users.noreply.github.com>
2022-09-20 09:52:39 +0200

    Update Boolean.cs

--- Open-Lab-01.05/Boolean.cs
+++ Open-Lab-01.05/Boolean.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._05
 {
@@ -6,6 +6,8 @@ namespace Open_Lab_01._05
     {
         public bool Reverse(bool value)
         {
+            return !value;
+
             throw new NotImplementedException();
         }
     }
